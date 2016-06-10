bad955ea83b97d79d2707ba9acfd76ec1d4245e1
andreabjork <andreabjork43@gmail.com>
2016-06-10 16:41:50 +0000

    Updated getAddress to now update QR code and button with the new address

--- getAddress.js
+++ getAddress.js
@@ -13,12 +13,37 @@ var getAddress = function() {
 		url:"https://smileyservice.herokuapp.com/generateAddress",
 		async:true,
 		type:"GET",
-		dataType:"json",
 		success: function(result){
-			alert(result);
+			var newAddress = extractAddress(result);
+			console.log("this is the new address "+newAddress);
+			var URL = makeNewUrl(newAddress);
+			updatePaymentButton(URL);
+			updateQRCode(URL);
 		},
 		error: function(xhr,ajaxOptions, thrownError){
 			console.log(xhr);
 		}
 	});
+}
+
+
+// Gets the address value from a JSON like {address: <somesmileycoinaddress>}
+var extractAddress = function(result) {
+	return ($.parseJSON(result)).address;
+}
+
+// Generates a new url/uri for the given address with amount 500000 smly:
+var makeNewUrl = function(address) {
+     return  "smileycoin:"+address+"?amount=500000.0&label=airfare";
+}
+
+// updates the payment button of the site with the new url
+var updatePaymentButton = function(url) {
+	$('#payWithSMLYURL').attr("href", url);
+}
+
+// updates the QR code of the site with the new url
+var updateQRCode = function(url) {
+	$('#qrcode')[0].innerHTML="";
+	new QRCode($('#qrcode')[0], url);
 }
