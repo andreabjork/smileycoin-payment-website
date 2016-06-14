6bb374ce245c3c0e9dcbe0820da3c319301606b2
andreabjork <andreabjork43@gmail.com>
2016-06-14 09:19:33 +0000

    Added error handling

--- getAddress.js
+++ getAddress.js
@@ -1,15 +1,13 @@
 
-var getAddress = function() {
+var getAddress = function(successCallback, errorCallback) {
 	$.ajax({
 		url:"https://smileyservice.herokuapp.com/generateAddress",
 		async:true,
 		type:"GET",
 		success: function(result){
-			var newAddress = extractAddress(result);
-			console.log("this is the new address " + newAddress);
-			var URL = makeNewUrl(newAddress);
-			updatePaymentButton(URL);
-			updateQRCode(URL);
+			// Handle a successful response based on whether the server
+			// actually managed to get an address or not. 
+			handleResponse(result, successCallback, errorCallback);
 		},
 		error: function(xhr,ajaxOptions, thrownError){
 			console.log(xhr);
@@ -17,11 +15,28 @@ var getAddress = function() {
 	});
 }
 
+var handleResponse = function(result, successCallback, errorCallback) {
+	if(jsonSuccess(result)) {
+		var newAddress = extractAddress(result);
+		console.log("this is the new address " + newAddress);
+		var URL = makeNewUrl(newAddress);
+		updatePaymentButton(URL);
+		updateQRCode(URL);
+		successCallback();
+	} else {
+		errorCallback();
+	}
+}
+
+var jsonSuccess = function(result) {
+	return (($.parseJSON(result)).message === "Success")
+}
 
 // Gets the address value from a JSON like {address: <somesmileycoinaddress>}
 var extractAddress = function(result) {
 	return ($.parseJSON(result)).address;
 }
+
 
 // Generates a new url/uri for the given address with amount 500000 smly:
 var makeNewUrl = function(address) {
